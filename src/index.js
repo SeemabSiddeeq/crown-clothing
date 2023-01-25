ab19515de24856295b801037c059a849e1874098
Seemab Siddeeq <mdseemab.sid@gmail.com>
2023-01-25 12:50:56 +0530

    added styled components

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
-import { CartProvider } from './contexts/cart.context';
+import ReactDOM from "react-dom/client";
 
-import './index.scss';
+import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById('root');
+import { BrowserRouter } from "react-router-dom";
 
-render(
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { CategoriesProvider } from "./contexts/categories.component";
+import { CartProvider } from "./contexts/cart.context";
+
+import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
