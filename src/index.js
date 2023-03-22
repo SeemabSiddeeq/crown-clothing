985cf51d18f4cb182563eda6afe7f5c30f97ec93
Seemab Siddeeq <mdseemab.sid@gmail.com>
2023-03-22 19:48:28 +0530

    added redux persist in store.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import { PersistGate } from "redux-persist/integration/react";
+
 import App from "./App";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -15,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
