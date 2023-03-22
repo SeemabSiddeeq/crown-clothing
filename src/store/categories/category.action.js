4949308e513fece3e688cf7b44a7d464ba889347
Seemab Siddeeq <mdseemab.sid@gmail.com>
2023-03-22 20:53:39 +0530

    added redux thunk in store

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,27 @@
 import CATEGORIES_ACTION_TYPES from "./cateogry.types";
 import createAction from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => {
+  return async (disptach) => {
+    disptach(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      disptach(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+      disptach(fetchCategoriesFailed(error));
+    }
+  };
+};
